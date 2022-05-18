82f28b5595b76c3c8b1a4e2429293881551cf613
daniel daum <daniel_daum@outlook.com>
2022-05-17 19:48:19 -0700

    added draft api routes and crud logic

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+
+from statistics import mode
+from fastapi import FastAPI, Depends, status, HTTPException
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from . import models
@@ -17,31 +19,73 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-@app.get("/user")
-def get_users(db: Session = Depends(get_db)):
+@app.get("/api/user", status_code=status.HTTP_200_OK)
+async def get_users(db: Session = Depends(get_db)):
+    """Returns all users in the database."""
 
     all_users = db.query(models.User).all()
     return {"data":all_users}
 
-@app.get("/user/{id}")
-def get_single_user(id: int, db: Session = Depends(get_db)):
 
 
-    # user = db.query(models.P)
-    return {"msg":"unfinished"}
+@app.get("/api/user/{id}",status_code=status.HTTP_200_OK)
+async def get_single_user(id: int, db: Session = Depends(get_db)):
+    """Returns a single user based on id."""
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+    return {"data":user}
 
 
-@app.post("/dogs")
-def create_posts(user:Test, db: Session = Depends(get_db)):
-    
 
+@app.post("/api/user", status_code=status.HTTP_201_CREATED)
+async def create_new_user(user:Test, db: Session = Depends(get_db)):
+    """Creates a new user in the database."""
     
     new_user= models.User(**user.dict())
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return {new_user}
+
+
+
+
+@app.delete("/api/user/{id}")
+async def delete_user(id:int, db: Session = Depends(get_db)):
+    """Deletes a user in the database based on id."""
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+
+    if user == None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+    db.delete(user)
+    db.commit()
+
+    return None
+
+
+
+
+@app.put("/api/user/{id}")
+async def update_user(id:int, db: Session = Depends(get_db) ):
+    """Updates all the attribue columns for a user based on id."""
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+
+    if user == None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+
+
 
 @app.get("/")
 async def root():
