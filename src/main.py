6f94870b8f90016324d72f00ace495d50f7a7e9f
Daniel Daum <84804774+daniel-daum@users.noreply.github.com>
2022-06-09 08:11:43 -0500

    Adding CORS to main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 from .database.database import engine
 from .database import models
 from .routes import users, supplies, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="LabFull API", version="0.0.1")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credential=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(supplies.router)
